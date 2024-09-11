ecf3c664ebdb6fdaad4adf3581be7060aca94c7a
aollivierre <abdullahollivierre@gmail.com>
2024-09-11 08:01:45 -0700

    adding updated module starter

--- Graph/0-BuildEntraAppRegCertBasedwithGraph-BETA.ps1
+++ Graph/0-BuildEntraAppRegCertBasedwithGraph-BETA.ps1
@@ -1,24 +1,46 @@
-# Fetch the script content
-$scriptContent = Invoke-RestMethod "https://raw.githubusercontent.com/aollivierre/module-starter/main/Module-Starter.ps1"
+# Set environment variable globally for all users
+[System.Environment]::SetEnvironmentVariable('EnvironmentMode', 'dev', 'Machine')
 
-# Define replacements in a hashtable
-$replacements = @{
-    '\$Mode = "dev"'                     = '$Mode = "dev"'
-    '\$SkipPSGalleryModules = \$false'   = '$SkipPSGalleryModules = $true'
-    '\$SkipCheckandElevate = \$false'    = '$SkipCheckandElevate = $true'
-    '\$SkipAdminCheck = \$false'         = '$SkipAdminCheck = $true'
-    '\$SkipPowerShell7Install = \$false' = '$SkipPowerShell7Install = $true'
-    '\$SkipModuleDownload = \$false'     = '$SkipModuleDownload = $true'
-    '\$SkipGitrepos = \$false'           = '$SkipGitrepos = $true'
+# Retrieve the environment mode (default to 'prod' if not set)
+$mode = $env:EnvironmentMode
+
+# Toggle based on the environment mode
+switch ($mode) {
+    'dev' {
+        Write-Host "Running in development mode" -ForegroundColor Yellow
+        # Your development logic here
+    }
+    'prod' {
+        Write-Host "Running in production mode" -ForegroundColor Green
+        # Your production logic here
+    }
+    default {
+        Write-Host "Unknown mode. Defaulting to production." -ForegroundColor Red
+        # Default to production
+    }
 }
 
-# Apply the replacements
-foreach ($pattern in $replacements.Keys) {
-    $scriptContent = $scriptContent -replace $pattern, $replacements[$pattern]
+#region FIRING UP MODULE STARTER
+#################################################################################################
+#                                                                                               #
+#                                 FIRING UP MODULE STARTER                                      #
+#                                                                                               #
+#################################################################################################
+
+# Define a hashtable for splatting
+$moduleStarterParams = @{
+    Mode                   = 'dev'
+    SkipPSGalleryModules   = $true
+    SkipCheckandElevate    = $true
+    SkipPowerShell7Install = $true
+    SkipEnhancedModules    = $true
+    SkipGitRepos           = $true
 }
 
-# Execute the script
-Invoke-Expression $scriptContent
+# Call the function using the splat
+Invoke-ModuleStarter @moduleStarterParams
+
+#endregion FIRING UP MODULE STARTER
 
 
 #!ToDO work on creatng a function for importing all modules in the modules folders without specifying the path of each module.
